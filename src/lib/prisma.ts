bd6de2c436b65e6b9882143bbd2c96dc2f80f80e
Abid <abidhussain0495@gmail.com>
2025-07-09 20:36:13 +0530

    Fixing prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,20 +2,23 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
     return new PrismaClient({
+        log: process.env.NODE_ENV === 'development' ? ['query'] : [],
         datasources: {
             db: {
-                url: process.env.DATABASE_URL + "&connection_limit=1" 
+                url: process.env.DATABASE_URL + (process.env.NODE_ENV === 'production' ? "&connection_limit=1" : "")
             }
         }
     })
 }
 
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
